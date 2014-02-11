fd14aacae07268ac75a48e641e06d34b2d47a478
Lette <christoffer.lette@gmail.com>
2014-02-11 22:18:40 +0100

    Version 1.

---
+++ Lette.Say/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Speech.Synthesis;
+
+namespace Lette.Say
+{
+    internal class Program
+    {
+        private static bool _echoLinesToConsole;
+
+        private static void Main(string[] args)
+        {
+            if (args[0].ToLower() == "-f")
+            {
+                if (args[0] == "-F")
+                {
+                    _echoLinesToConsole = true;
+                }
+
+                SpeakFile(args[1]);
+            }
+            else
+            {
+                SpeakLine(string.Join(" ", args));
+            }
+        }
+
+        private static void SpeakLine(string textToSpeak)
+        {
+            using (var speech = new SpeechSynthesizer())
+            {
+                speech.Speak(textToSpeak);
+            }
+        }
+
+        private static void SpeakFile(string fileName)
+        {
+            foreach (var line in File.ReadLines(fileName))
+            {
+                if (_echoLinesToConsole)
+                {
+                    Console.Out.WriteLine(line);
+                }
+                SpeakLine(line);
+            }
+        }
+    }
+}
---
+++ Lette.Say/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("Say")]
+[assembly: AssemblyDescription("Say Executable")]
+[assembly: AssemblyCompany("Christoffer Lette")]
+[assembly: AssemblyProduct("Say")]
+[assembly: AssemblyCopyright("Copyright © Christoffer Lette 2014-")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
